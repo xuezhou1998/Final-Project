7cf34d49b8783afc7c263ab658abf3376f1bf7ca
xuezhou1998 <xuezhou1998@gmail.com>
2021-11-13 04:39:51 -0500

    wrote some parts in main

--- Main.py
+++ Main.py
@@ -7,9 +7,57 @@ import Transaction_Manager
 import Variable
 
 
-def main():
+def main(input_file_path):
+
     # print("Hello World!")
-    pass
+    trans_mgr=Transaction_Manager()
+    cmmd_waitlist=[]
+    input_path=input_file_path
+    exe_result, in_waitlist=False,False
+    waitlist_idx=0
+    
+
+    try:
+        file_read= open(input_path,"r")
+        while True:
+
+            fetched=None
+            in_waitlist=False
+
+            if (len(cmmd_waitlist)>0 and waitlist_idx< len(cmmd_waitlist)):
+                print("waitlist_idx %d",waitlist_idx)
+                fetched=cmmd_waitlist[waitlist_idx]
+                in_waitlist=True
+            else:
+                qry=file_read.readline()
+                if qry!=None:
+                    fetched=Query_Parser.parse_query(fetched)
+                else:
+                    break
+            
+            if (fetched[0]=='begin'):
+                pass
+            elif (fetched[0]==''):
+                pass
+            elif (fetched[0]==''):
+                pass
+            elif (fetched[0]==''):
+                pass
+            elif (fetched[0]==''):
+                pass
+            elif (fetched[0]==''):
+                pass
+            elif (fetched[0]==''):
+                pass
+            elif (fetched[0]==''):
+                pass
+    except:
+        print("Something went wrong")
+    finally:
+        print("The 'try except' is finished")
+
+
+    
 
 
 
--- Query_Parser.py
+++ Query_Parser.py
@@ -1,3 +1,8 @@
+from typing import List
+
+
 class Query_Parser:
     def __init__(self) -> None:
+        pass
+    def parse_query(input_query) -> List:
         pass
