3adb9b8e4070c2a76da10cb1703f384f789e6090
xuezhou1998 <xuezhou1998@gmail.com>
2021-11-15 03:49:37 -0500

    main completed 1

--- Main.py
+++ Main.py
@@ -36,23 +36,40 @@ def main(input_file_path):
                     break
             
             if (fetched[0]=='begin'):
-                pass
-            elif (fetched[0]==''):
-                pass
-            elif (fetched[0]==''):
-                pass
-            elif (fetched[0]==''):
-                pass
-            elif (fetched[0]==''):
-                pass
-            elif (fetched[0]==''):
-                pass
-            elif (fetched[0]==''):
-                pass
-            elif (fetched[0]==''):
-                pass
+                exe_result=trans_mgr.begin(int(fetched[1]))
+            elif (fetched[0]=='beginRO'):
+                exe_result=trans_mgr.begin_read_only(int(fetched[1]))
+            elif (fetched[0]=='R'):
+                exe_result=trans_mgr.read(int(fetched[1]),int(fetched[2]))
+            elif (fetched[0]=='W'):
+                exe_result=trans_mgr.write(int(fetched[1]),int(fetched[2]),int(fetched[3]))
+            elif (fetched[0]=='recover'):
+                exe_result=trans_mgr.recover(int(fetched[1]))
+            elif (fetched[0]=='fail'):
+                exe_result=trans_mgr.fail(int(fetched[1]))
+            elif (fetched[0]=='dump'):
+                exe_result=trans_mgr.dump()
+            elif (fetched[0]=='end'):
+                exe_result=trans_mgr.end(int(fetched[1]))
+            
+            if exe_result==True:
+                if in_waitlist==True:
+                    cmmd_waitlist.remove(waitlist_idx)
+                waitlist_idx=0
+            else:
+                if in_waitlist==False:
+                    cmmd_waitlist.append(fetched)
+                    waitlist_idx=0
+                else:
+                    waitlist_idx+=1
+            trans_mgr.time_stamp+=1
+            trans_mgr.dead_lock_detect()
+
+
+
     except:
         print("Something went wrong")
+        
     finally:
         print("The 'try except' is finished")
 
--- Transaction_Manager.py
+++ Transaction_Manager.py
@@ -1,3 +1,56 @@
+import Data_Manager
+import Lock
+import Query_Parser
+import Site
+import Transaction
+import Transaction_Manager
+import Variable
+
 class Transaction_Manager:
     def __init__(self) -> None:
+        self.time_stamp=0
+        self.data_mgr=Data_Manager()
+        self.trans_map={}##integer: transaction
+        
+    def get(trans_id):
         pass
+    def begin(trans_id):
+        pass
+    def begin_read_only(trans_id):
+        pass
+    def read(trans_id):
+        pass
+
+    def read_read_only(trans_id):
+        pass
+    def write(trans_id):
+        pass
+    def block_trans(trans_id):
+        pass
+    def end(trans_id):
+        pass
+    def dump(trans_id):
+        pass
+    def get_read_lock(trans_id):
+        pass
+    def dead_lock_detect():
+        pass
+    def find_yongest(trans_id):
+        pass
+    def abort_trans_multi(trans_id):
+        pass
+    def abort_trans(trans_id):
+        pass
+    def release_locks(trans_id):
+        pass
+    def unblock_trans(trans_id):
+        pass
+    def trans_init_checker(trans_id):
+        pass
+    def alive_checker(trans_id):
+        pass
+    def recover(trans_id):
+        pass
+    def fail(trans_id):
+        pass
+    
