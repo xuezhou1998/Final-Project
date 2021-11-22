ffeb20689745fb1beb9b3527173a66680e3d56ac
Lianghao Shu <shulh8@gmail.com>
2021-11-22 00:22:30 -0500

    Lock class

--- Lock.py
+++ Lock.py
@@ -1,3 +1,7 @@
 class Lock:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,  Type_of_Lock, Get_Time, Transaction_ID):
+        self.Type_of_Lock = Type_of_Lock
+        self.Get_Time = Get_Time
+        self.Transaction_ID = Transaction_ID
+        self.siteID = 0
+        varId = 0
